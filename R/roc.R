86178db002958b2d8ad908f116e0604695494699
markvdwiel <mark.vdwiel@vumc.nl>
2018-01-22 16:07:31 +0100

    some changes

--- R/roc.R
+++ R/roc.R
@@ -1,5 +1,10 @@
 roc <-
 function(probs,true,cutoffs){  
+    if(cutoffs[2] >= cutoffs[1]){
+      print("ERROR: cut-offs needs to be a DECREASING vector")
+      return(NULL)
+    } else {
     rocs <- sapply(cutoffs, .rocaux, probs=probs, true=true)
     return(rocs)
+    }
 }
